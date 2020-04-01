8dfa194d99b0b008eb6d2e5f455f5a98e20d4173
jbrophy <jonathanbrophy47@gmail.com>
2020-03-31 21:55:58 -0700

    updated performance experiment for trees

--- experiments/scripts/tree/performance.py
+++ experiments/scripts/tree/performance.py
@@ -7,6 +7,7 @@ import time
 import argparse
 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
 
 here = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, here + '/../../../')
@@ -17,6 +18,9 @@ from utility import data_util, exp_util, print_util
 
 def performance(args, logger, seed):
 
+    param_grid = {'max_depth': [1, 3, 5, 10, 20]}
+    logger.info('param_grid: {}'.format(param_grid))
+
     # obtain data
     X_train, X_test, y_train, y_test = data_util.get_data(args.dataset, seed, data_dir=args.data_dir)
 
@@ -25,32 +29,56 @@ def performance(args, logger, seed):
     logger.info('test instances: {:,}'.format(X_test.shape[0]))
     logger.info('attributes: {:,}'.format(X_train.shape[1]))
 
-    logger.info('building sk_tree...')
+    # SKLearn
+    logger.info('\nSKLearn')
     start = time.time()
-    sk_tree = DecisionTreeClassifier(max_depth=args.max_depth, random_state=seed)
-    sk_tree = sk_tree.fit(X_train, y_train)
-    logger.info('{:.3f}s'.format(time.time() - start))
+    model = DecisionTreeClassifier(max_depth=args.max_depth, random_state=seed)
 
-    logger.info('building d_tree...')
+    if args.tune:
+        gs = GridSearchCV(model, param_grid, scoring=args.scoring, cv=args.cv, verbose=args.verbose)
+        gs = gs.fit(X_train, y_train)
+        model = gs.best_estimator_
+        logger.info('best_params: {}'.format(gs.best_params_))
+    else:
+        model = model.fit(X_train, y_train)
+
+    logger.info('{:.3f}s'.format(time.time() - start))
+    exp_util.performance(model, X_test, y_test, name='sklearn', logger=logger)
+
+    # Exact
+    logger.info('\nExact')
     start = time.time()
-    d_tree = cedar.Tree(epsilon=args.epsilon, lmbda=1000000000, max_depth=args.max_depth,
-                        verbose=args.verbose, random_state=seed)
-    d_tree = d_tree.fit(X_train, y_train)
+    model = cedar.Tree(epsilon=args.epsilon, lmbda=-1, max_depth=args.max_depth,
+                       verbose=args.verbose, random_state=seed)
+
+    if args.tune:
+        gs = GridSearchCV(model, param_grid, scoring=args.scoring, cv=args.cv, verbose=args.verbose)
+        gs = gs.fit(X_train, y_train)
+        model = gs.best_estimator_
+        logger.info('best_params: {}'.format(gs.best_params_))
+    else:
+        model = model.fit(X_train, y_train)
+
     logger.info('{:.3f}s'.format(time.time() - start))
+    exp_util.performance(model, X_test, y_test, name='exact', logger=logger)
 
-    logger.info('building CeDAR tree...')
-    start = time.time()
-    dt_tree = cedar.Tree(epsilon=args.epsilon, lmbda=args.lmbda,
-                         max_depth=args.max_depth, verbose=args.verbose, random_state=seed)
-    dt_tree = dt_tree.fit(X_train, y_train)
-    logger.info('{:.3f}s'.format(time.time() - start))
+    # CeDAR
+    if args.cedar:
+        logger.info('\nCeDAR')
+        start = time.time()
+        model = cedar.Tree(epsilon=args.epsilon, lmbda=args.lmbda,
+                           max_depth=args.max_depth, verbose=args.verbose, random_state=seed)
 
-    dt_tree.print()
+        if args.tune:
+            gs = GridSearchCV(model, param_grid, scoring=args.scoring, cv=args.cv, verbose=args.verbose)
+            gs = gs.fit(X_train, y_train)
+            model = gs.best_estimator_
+            logger.info('best_params: {}'.format(gs.best_params_))
+        else:
+            model = model.fit(X_train, y_train)
 
-    # display performance
-    exp_util.performance(sk_tree, X_test, y_test, name='sk_tree', logger=logger)
-    exp_util.performance(d_tree, X_test, y_test, name='d_tree', logger=logger)
-    exp_util.performance(dt_tree, X_test, y_test, name='cedar_tree', logger=logger)
+        logger.info('{:.3f}s'.format(time.time() - start))
+        exp_util.performance(model, X_test, y_test, name='cedar', logger=logger)
 
 
 def main(args):
@@ -72,10 +100,19 @@ if __name__ == '__main__':
     parser.add_argument('--data_dir', type=str, default='data', help='data directory.')
     parser.add_argument('--out_dir', type=str, default='output/tree/performance', help='output directory.')
     parser.add_argument('--dataset', default='mfc19', help='dataset to use for the experiment.')
+
     parser.add_argument('--rs', type=int, default=1, help='random state.')
-    parser.add_argument('--epsilon', type=float, default=1.0, help='idistinguishability parameter.')
+
+    parser.add_argument('--tune', action='store_true', default=True, help='tune models.')
+    parser.add_argument('--cv', type=int, default=3, help='number of cross-validation folds for tuning.')
+    parser.add_argument('--scoring', type=str, default='accuracy', help='metric for tuning.')
+
+    parser.add_argument('--cedar', action='store_true', default=False, help='run CeDAR model.')
+    parser.add_argument('--epsilon', type=float, default=1.0, help='indistinguishability parameter.')
     parser.add_argument('--lmbda', type=float, default=100, help='amount of noise to add.')
+
     parser.add_argument('--max_depth', type=int, default=None, help='maximum depth of the tree.')
+
     parser.add_argument('--verbose', type=int, default=0, help='verbosity level.')
     args = parser.parse_args()
     main(args)
--- readme.md
+++ readme.md
@@ -5,7 +5,8 @@
 Getting Started
 ---
 1. Clone or fork this repository.
-2. Install dependencies:
-    1. Install 3rd party Python3 modules: `pip3 install -a 'requirements.txt'`.
+2. Install Python >= 3.7.3.
+3. Install dependencies:
+    1. Install 3rd party Python3 modules: `pip3 install -r requirements.txt`.
     2. Build CeDAR module: `cd cedar && python3 setup.py build_ext --inplace && cd ..`.
-3. Look at `tests/tree.py` or `tests/forest.py` for example usage!
+4. Look at `tests/tree.py` or `tests/forest.py` for example usage!
