24aedc6fd7161fb37c240be3d904b71e5109a9c7
jbrophy <jonathanbrophy47@gmail.com>
2020-05-14 12:45:03 -0700

    switched to entropy

--- jobs/delete_until_retrain/higgs2.sh
+++ jobs/delete_until_retrain/higgs2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=long
 #SBATCH --job-name=delete_until_retrain
-#SBATCH --output=jobs/logs/delete_until_retrain/higgs2_gi
-#SBATCH --error=jobs/errors/delete_until_retrain/higgs2_gi
+#SBATCH --output=jobs/logs/delete_until_retrain/higgs2_en
+#SBATCH --error=jobs/errors/delete_until_retrain/higgs2_en
 #SBATCH --time=5-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -14,9 +14,9 @@ dataset="higgs"
 n_estimators=100
 max_depth=10
 max_features=0.25
-lmbdas=(340 360)
+lmbdas=(350 400)
 frac_remove=0.35
-criterion="gini"
+criterion="entropy"
 
 data_dir="data/"
 out_dir="output/delete_until_retrain/"
--- jobs/delete_until_retrain/skin1.sh
+++ jobs/delete_until_retrain/skin1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=long
 #SBATCH --job-name=delete_until_retrain
-#SBATCH --output=jobs/logs/delete_until_retrain/skin1_gi
-#SBATCH --error=jobs/errors/delete_until_retrain/skin1_gi
+#SBATCH --output=jobs/logs/delete_until_retrain/skin1_en
+#SBATCH --error=jobs/errors/delete_until_retrain/skin1_en
 #SBATCH --time=7-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -16,7 +16,7 @@ max_depth=20
 max_features=-1
 lmbdas=(0 0 0)
 frac_remove=0.35
-criterion="gini"
+criterion="entropy"
 
 data_dir="data/"
 out_dir="output/delete_until_retrain/"
