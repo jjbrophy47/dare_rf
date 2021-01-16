f05283ba6805aeba7f955b725cf63824086aa902
jbrophy <jonathanbrophy47@gmail.com>
2021-01-16 15:26:46 -0800

    updating more datasets

--- data/gas_sensor/readme.md
+++ data/gas_sensor/readme.md
@@ -1,6 +1,6 @@
 Gas Sensor Dataset
 ---
-* Download the following files from [https://archive.ics.uci.edu/ml/datasets/Gas+sensors+for+home+activity+monitoring](https://archive.ics.uci.edu/ml/datasets/Gas+sensors+for+home+activity+monitoring) to this directory.
+* Download the following files from [https://archive.ics.uci.edu/ml/machine-learning-databases/00362/](https://archive.ics.uci.edu/ml/machine-learning-databases/00362/) to this directory.
 	* `HT_Sensor_UCIsubmission.zip`, then unzip this file.
 
 * Preprocess the data.
---
+++ data/skin/continuous.py
@@ -0,0 +1,79 @@
+"""
+Preprocesses dataset but keep continuous variables.
+"""
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+
+def dataset_specific(random_state, test_size):
+
+    # retrieve dataset
+    df = pd.read_csv('Skin_NonSkin.txt', header=None, sep='\t')
+    df.columns = ['b', 'g', 'r', 'label']
+
+    # remove nan rows
+    nan_rows = df[df.isnull().any(axis=1)]
+    print('nan rows: {}'.format(len(nan_rows)))
+    df = df.dropna()
+
+    # remove columns
+    remove_cols = []
+    df = df.drop(remove_cols, axis=1)
+
+    # split into train and test
+    indices = np.arange(len(df))
+    n_train_samples = int(len(indices) * (1 - test_size))
+
+    np.random.seed(random_state)
+    train_indices = np.random.choice(indices, size=n_train_samples, replace=False)
+    test_indices = np.setdiff1d(indices, train_indices)
+
+    train_df = df.iloc[train_indices]
+    test_df = df.iloc[test_indices]
+
+    # categorize attributes
+    columns = list(df.columns)
+    label = ['label']
+    numeric = ['b', 'g', 'r']
+    categorical = list(set(columns) - set(numeric) - set(label))
+
+    return train_df, test_df, label, numeric, categorical
+
+
+def main(random_state=1, test_size=0.2, out_dir='continuous'):
+
+    train_df, test_df, label, numeric, categorical = dataset_specific(random_state=random_state,
+                                                                      test_size=test_size)
+
+    # binarize inputs
+    ct = ColumnTransformer([('kbd', 'passthrough', numeric),
+                            ('ohe', OneHotEncoder(sparse=False, handle_unknown='ignore'), categorical)])
+    train = ct.fit_transform(train_df)
+    test = ct.transform(test_df)
+
+    # binarize outputs
+    le = LabelEncoder()
+    train_label = le.fit_transform(train_df[label].to_numpy().ravel()).reshape(-1, 1)
+    test_label = le.transform(test_df[label].to_numpy().ravel()).reshape(-1, 1)
+
+    # combine binarized data
+    train = np.hstack([train, train_label]).astype(np.int32)
+    test = np.hstack([test, test_label]).astype(np.int32)
+
+    print('train.shape: {}, label sum: {}'.format(train.shape, train[:, -1].sum()))
+    print('test.shape: {}, label sum: {}'.format(test.shape, test[:, -1].sum()))
+
+    # save to numpy format
+    print('saving...')
+    os.makedirs(out_dir, exist_ok=True)
+    np.save(os.path.join(out_dir, 'train.npy'), train)
+    np.save(os.path.join(out_dir, 'test.npy'), test)
+
+
+if __name__ == '__main__':
+    main()
